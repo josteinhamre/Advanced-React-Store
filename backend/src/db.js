661ec3c6149a790c1f8c7cf53ee1a02119942a3e
Jostein Hamre <josteinhamre@gmail.com>
2019-11-29 14:41:41 +0100

    Password reset mail, and indent change.

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,10 +2,10 @@
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
-    endpoint: process.env.PRISMA_ENDPOINT,
-    secret: process.env.PRISMA_SECRET,
-    debug: true,
-})
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: true,
+});
 
 module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: '.env' });
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
-const jwt = require('jsonwebtoken');
-
 
 const server = createServer();
 
@@ -12,19 +11,22 @@ server.express.use(cookieParser());
 // Decode jwt to get user id
 
 server.express.use((req, res, next) => {
-    const { token } = req.cookies;
-    if (token) {
-        const { userId } = jwt.verify(token, process.env.APP_SECRET);
-        req.userId = userId
-    }
-    next();
-})
-
-server.start({
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
-    },
-}, deets => {
-    console.log(`Server is now running on http://localhost:${deets.port}`)
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
 });
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on http://localhost:${deets.port}`);
+  }
+);
