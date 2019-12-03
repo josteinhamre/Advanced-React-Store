b99d6894f958c23b93773c950a0568018b887a93
Jostein Hamre <josteinhamre@gmail.com>
2019-12-04 00:16:23 +0100

    Downshift implemented

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "201bc1d4d8b7dabd2a82";
+/******/ 	var hotCurrentHash = "5831804c1e7128b823fc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
